5ffef7d26c33947f28e2686580dd9f4803fdf706
deostroll <deostroll@gmail.com>
2015-10-27 08:46:24 +0530

    quadratic curve fitting as per http://stackoverflow.com/a/7058606/145682

--- src/main.js
+++ src/main.js
@@ -1,17 +1,3 @@
-
-var stage = new Konva.Stage({
-  container:'container',
-  height: 400,
-  width: 400
-});
-
-var max = {
-  x: stage.getWidth(),
-  y: stage.getHeight()
-};
-
-var layer = new Konva.Layer();
-
 (function () {
   Konva.Point = function(config) {
     this.___init(config);
@@ -38,96 +24,51 @@ var layer = new Konva.Layer();
 
   Konva.Util.extend(Konva.Point, Konva.Shape);
 })();
-
-var point = new Konva.Point({
-  x: stage.getWidth()/2,
-  y: stage.getHeight()/2,
-  stroke: 'red'
+var stage = new Konva.Stage({
+  container:'container',
+  height: 400,
+  width: 400
 });
 
-var rect = new Konva.Rect({
-    height: stage.getHeight(),
-    width: stage.getWidth(),
-    stroke: 'black',
-    x:0,
-    y:0
-});
-
-var xaxis = new Konva.Line({
-  x: 0,
-  y: max.y/2,
-  points: [
-    0,0,
-    max.x, 0],
-  stroke:'black',
-  strokeWidth:0.1
-});
-
-var yaxis = new Konva.Line({
-  x: max.x/2,
-  y: 0,
-  points: [
-    0,0,
-    0,max.y],
-  stroke:'black',
-  strokeWidth:0.1
-});
-
-var gorigin = {
-  x: max.x/2,
-  y: max.y/2
+var max = {
+  x: stage.getWidth(),
+  y: stage.getHeight()
 };
 
-var scale = {
-  x: 1,
-  y: 1
-};
-
-function translateToPoint(rpoint) {
-  var xoffset = rpoint.x/scale.x + gorigin.x;
-  var yoffset = gorigin.y - rpoint.y/scale.y;
-  return new Konva.Point({
-    x: xoffset,
-    y: yoffset,
-    stroke:'transparent',
-    opacity: '0.5'
-  });
-}
-var f = function(radius, angle) {
-  return {
-    x: radius * Math.cos(angle * Math.PI/180),
-    y: radius * Math.sin(angle * Math.PI/180)
-  };
-};
-
-var parabola = function(t) {
-  return {x: t, y: t*t};
-};
-
-var cubic = function(t) {
-  return {x: t, y: (t-10) * (t+2) * (t+30) };
-}
-
-var cycloid = function(r, t) {
-  return {
-    x: r * (t - Math.sin(t)),
-    y: r * (1 - Math.cos(t))
-  };
-}
-for(var i = -50; i <=50; i++ ) {
-
-  var pt = translateToPoint(cycloid(25, i));
-  layer.add(pt);
-}
-var points = layer.find('Point');
-var pline = new Konva.Line({
-  stroke: 'pink',
-  stokeWidth: 1,
-  points: points.reduce(function(a, c){ return a.concat([c.x(), c.y()])}, []),
-  x: 0,
-  y: 0,
-  tension: 0.33
-});
-
-layer.add(rect, point, xaxis, yaxis, pline);
+var layer = new Konva.Layer();
+var points = [];
 stage.add(layer);
+stage.on('contentClick', function(){
+  var pos = stage.getPointerPosition();
+  var pt = new Konva.Point({
+    x: pos.x,
+    y: pos.y,
+    stroke: 'blue'
+  });
+  layer.add(pt);
+  points.push(pt);
+  layer.draw();
+});
+
+document.getElementById('btnDraw')
+  .addEventListener('click', function(){
+    drawCurve();
+  }, false);
+
+function drawCurve() {
+  var ctx = layer.getContext();
+  if(points.length > 1) {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'pink';
+    ctx.moveTo(points[0].x(), points[0].y());
+    for (var i = 0, j = points.length - 2; i < j; i++) {
+      var p1 = points[i],
+        p2 = points[i+1];
+      var xc = (p1.x() + p2.x()) / 2;
+      var yc = (p1.y() + p2.y()) / 2;
+      ctx.quadraticCurveTo(p1.x(), p1.y(), xc,yc);
+    }
+    ctx.stroke();
+  }
+}
